59d365ca26080f7cd14df6cafca4671cd2a0c63c
EmirhanImranTuzel <emrhntzl@gmail.com>
2024-01-10 21:28:11 +0100

    user data was improved

--- With_Aws_Cli/Auto_Public_Ip_Pull/user_data.sh
+++ With_Aws_Cli/Auto_Public_Ip_Pull/user_data.sh
@@ -17,3 +17,6 @@ echo "PS1='\[\e[0;31m\]\u\[\e[m\] \[\e[0;32m\]\h\[\e[m\]@\[\e[0;34m\]\w\[\e[m\]\
 sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
 sudo yum -y install terraform
+
+# Install Docker;
+
