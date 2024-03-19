838bada123367845bc64940ae9f06764d49f4043
EmirhanImranTuzel <emrhntzl@gmail.com>
2024-03-19 16:43:51 +0100

    user data was improved

--- With_Aws_Cli/Auto_Public_Ip_Pull/user_data.sh
+++ With_Aws_Cli/Auto_Public_Ip_Pull/user_data.sh
@@ -7,23 +7,18 @@ echo 'export LANG=en_US.UTF-8' >> /etc/environment
 echo 'export LC_COLLATE=C' >> /etc/environment
 echo 'export LC_CTYPE=en_US.UTF-8' >> /etc/environment
 
-sudo yum update -y && sudo yum upgrade -y
+yum update -y && yum upgrade -y
 
 echo "PS1='\[\e[0;31m\]\u\[\e[m\] \[\e[0;32m\]\h\[\e[m\]@\[\e[0;34m\]\w\[\e[m\]\$ '" >> ~/.bashrc && source ~/.bashrc
 
 
 # Install Terraform;
 
-sudo yum install -y yum-utils
-sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
-sudo yum -y install terraform
+yum install -y yum-utils
+yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
+yum -y install terraform
+amazon-linux-extras install docker -y
+systemctl start docker
+systemctl enable docker
+usermod -a -G docker ec2-user
 
-# Install Docker;
-
-sudo amazon-linux-extras install docker -y
-sudo systemctl start docker
-sudo systemctl enable docker
-sudo usermod -a -G docker ec2-user
-newgrp docker
-curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod -v +x /usr/local/bin/docker-compose
